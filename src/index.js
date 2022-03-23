9d9f9967139e805a0585dc06ed76fd38678968a4
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-03-23 19:11:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Rotas
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./Router/routes";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Router />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
