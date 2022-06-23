a3e726deb6f2b42d1c16acce7c648be0738c68ef
João Ygor <ygorvieira2002@gmail.com>
2022-06-23 15:23:36 -0400

    primary commit

---
+++ pets/src/pages/_app.js
@@ -0,0 +1,8 @@
+import "../ui/styles/globals.css";
+import { themeProvider } from "@mui/material";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
