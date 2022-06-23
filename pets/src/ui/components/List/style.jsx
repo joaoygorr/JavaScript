19f2e390bce81be7e65e896fc47e09633b5fc957
João Ygor <ygorvieira2002@gmail.com>
2022-06-23 18:28:20 -0400

    style list

--- pets/src/ui/components/List/style.jsx
+++ pets/src/ui/components/List/style.jsx
@@ -1,10 +1,21 @@
 import { styled } from "@mui/material";
 
-export const ListStyled = styled("ul")``;
+export const ListStyled = styled("ul")`
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.spacing(2)};
+`;
 
-export const ItenList = styled("li")``;
+export const ItenList = styled("li")`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${({ theme }) => theme.spacing(5)};
+`;
 
-export const Photo = styled("img")``;
+export const Photo = styled("img")`
+  width: 100%;
+`;
 
 export const Information = styled("div")``;
 
