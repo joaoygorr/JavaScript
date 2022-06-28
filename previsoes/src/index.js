57150f358e5331d877af7c3be406f70203bd8b63
João Ygor <ygorvieira2002@gmail.com>
2022-06-28 16:21:00 -0400

    primary commit

---
+++ previsoes/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Page
+import Home from "./Pages/Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
